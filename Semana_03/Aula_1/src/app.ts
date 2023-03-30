6f8af1f35b7a8e275de0504b8bc3b3e04b7f506e
Ana Maria De Sousa Carvalho <ana.sousa@MJV.net>
2023-03-30 04:46:42 -0300

    Aula 3 - Semana 3

--- Semana_03/Aula_1/src/app.ts
+++ Semana_03/Aula_1/src/app.ts
@@ -1,25 +1,17 @@
 import  express  from "express";
-import { Request, Response, Router } from "express";
 import cors from 'cors';
-
+import routes from './routers';
 
 const app = express();
 
 app.use(cors()); 
 app.use(express.json());
-
-const router = Router();
-
-
-router.get('/', (req: Request, res: Response) => {
-    const helloworld = {message: 'Hello World!'};
-    res.send(helloworld);
-});
-
-app.use(router);
+app.use(routes);
 
 const port = 3000;
 
 app.listen(port, () => {
     console.log('Aplicaçao online na porta: ', port);
 });
+
+
